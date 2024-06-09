95825b30eb60c5209623c41149e7fd0e59f64afc
Divya <divyamakkar000@gmail.com>
2024-06-09 10:32:44 -0400

    june 9 p1

--- balancedRatingChanges.cpp
+++ balancedRatingChanges.cpp
@@ -1,36 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     //taking in inputs
-    int n; 
+    int n;
     cin >> n;
 
-    int sum = 0;
     int a[n];
 
-    for (int i = 0 ; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    for (int i = 0 ; i < n; i++){
-        if (a[i] % 2 == 0){
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) {
             a[i] /= 2;
-            sum -= a[i];
-        }
-    }
-
-    for (int i = 0; i < n; i++){
-        if (a[i] % 2 != 0 && sum < 0){
-            a[i] = ceil((double)a[i] / 2.0); 
-        } else if (a[i] % 2 != 0 && sum > 0){
-            a[i] = floor((double)a[i] / 2.0); 
+        } else {
+            if (a[i] < 0) {
+                if (count % 2 != 0){
+                    a[i] = ((a[i] - 1) / 2); // Adjust for negative odd numbers
+                } else {
+                    a[i] = ((a[i]) / 2);
+                }
+            } else {
+                if (count % 2 == 0){
+                    a[i] = ((a[i] + 1) / 2);
+                } else {
+                    a[i] = ((a[i]) / 2);
+                }
+            }
+            count++;
         }
         cout << a[i] << endl;
     }
 
-
-
-
+    return 0;
 }
