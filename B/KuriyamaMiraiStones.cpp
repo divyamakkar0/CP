0e79401dfbbc3ec69cb4375e8ad05d41eadcea46
Divya <divyamakkar000@gmail.com>
2024-06-22 09:36:11 -0400

    june 22 p1

--- B/KuriyamaMiraiStones.cpp
+++ B/KuriyamaMiraiStones.cpp
@@ -8,9 +8,11 @@ using namespace std;
 
 int main()
 {
+    //take in n
     long long int n;
     cin >> n;
 
+    //take in costs
     vector<long long int> costs;
     vector<long long int> sorted;
     for (long long int i = 0; i < n; i++)
@@ -21,32 +23,42 @@ int main()
         sorted.push_back(x);
     }
 
-    sort(sorted.begin(), sorted.end());
+    sort(sorted.begin(),sorted.end());
 
-
+    //take in q
     long long int m;
     cin >> m;
-    long long int sum = 0;
 
-    vector<vector<long long int>> q(m, vector<long long int>(3));
-    for (long long int i = 0; i < m; ++i) {
-        for (long long int j = 0; j < 3; ++j) {
-            cin >> q[i][j];  
-        }
+    //prefix sum costs
+    vector<long long int> ps1(n);
+    ps1[0] = costs[0];
+    for(int i = 1; i < n;i++){
+        ps1[i] = ps1[i-1] + costs[i];
+    }
 
-         if(q[i][0] == 2){
-                for(long long int k = q[i][1]-1; k <= q[i][2]-1; k++){
-                    sum += sorted[k];
-                }
-                cout << sum << endl;
-                sum = 0;
+    ps1.insert(ps1.begin(), 0);
+
+    //prefix sum sorted
+    vector<long long int> ps2(n);
+    ps2[0] = sorted[0];
+    for(int i = 1; i < n;i++){
+        ps2[i] = ps2[i-1] + sorted[i];
+    }
+
+    ps2.insert(ps2.begin(), 0);
+
+
+    for(int i = 0; i < m; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        long long int sum = 0;
+        //sorted ps2
+        if (a == 2){
+            sum = ps2[c] - ps2[b-1];
         } else {
-                for(long long int k = q[i][1]-1; k <= q[i][2]-1; k++){
-                    sum += costs[k];
-                }
-                cout << sum << endl;
-                sum = 0;
+            sum = ps1[c] - ps1[b-1];
         }
+        cout << sum << endl;
     }
 
 }
