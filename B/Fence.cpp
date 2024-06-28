2c62e44fa41923588b4882a8d292f71826b692fc
Divya <divyamakkar000@gmail.com>
2024-06-27 22:02:27 -0400

    june 27

--- B/Fence.cpp
+++ B/Fence.cpp
@@ -11,9 +11,32 @@ int main(){
     cin >> n >> k;
 
     vector<int> h(n);
+    
     for(int i = 0; i <n; i++){
         cin >> h[i];
     }
+    int sum = 100000;
+    int index = 0;
+    //something about sum of k here
+    for(int i = 0; i < n-(k-1); i++){
+        int p = i;
+        int k2 = k;
+        int temp = 0;
 
+        while(k2){
+            temp += h[i];
+            i++;
+            k2--;
+        }
+        // cout << "temp" << temp << endl;
+        if (temp < sum){
+            sum = temp;
+            index = p;
+        }
+        i = p;
+        // cout << "p " << p << endl;
+    }
+
+    cout << index +1; 
     
 }
