a2bc3af5c26e61ddd4a10f3867891f72d34e5b63
Divya <divyamakkar000@gmail.com>
2024-07-02 09:02:09 -0400

    p2

---
+++ B/SummerSellOff.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+    int n, f;
+    cin >> n >> f;
+
+    vector<pair<int, int>> d;
+    
+    for (int i = 0; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+        d.push_back(make_pair(a, b));
+    }
+
+    
+
+}
