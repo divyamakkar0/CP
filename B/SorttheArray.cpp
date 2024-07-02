7828e1c6f4c4d86f682b370e88ce3a151ed76d61
Divya <divyamakkar000@gmail.com>
2024-07-02 08:42:24 -0400

    july 2

--- B/SorttheArray.cpp
+++ B/SorttheArray.cpp
@@ -13,18 +13,51 @@ int main()
 
     vector<int> d(n);
 
+    int r = -1;
+    int l = -1;
+
     for (int i = 0; i < n; i++)
     {
         cin >> d[i];
-    }   
+    }  
 
-    int p = 0;
-    for (int i = 0; i < n; i++)
+    //find starting
+    for (int i = 0; i < n - 1; i++)
     {
-        if(d[i] - d[i+1]==1){
-            p++;
+        if (d[i] > d[i+1]){
+            l = i;
+            break;
         }
-        
     }
 
+    //find ending
+    for (int i = n-1; i > 0 ; i--){
+        if(d[i] < d[i-1]){
+            r = i;
+            break;
+        }
+    }
+
+    if (l == -1){
+        cout << "yes" << endl;
+        cout << "1 1";
+    } else {
+
+    reverse(d.begin() + l, d.begin() + r + 1);
+
+    bool sorted = true;
+    for (int i = 0; i < n - 1; ++i) {
+        if (d[i] > d[i + 1]) {
+            sorted = false;
+            break;
+        }
+    }
+
+    if (sorted) {
+        cout << "yes" << endl;
+        cout << l + 1 << " " << r + 1 << endl;
+    } else {
+        cout << "no" << endl;
+    }
+    }
 }
