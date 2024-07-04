b05e2ec20d50fe05a17badeca0d84a8676429097
Divya <divyamakkar000@gmail.com>
2024-07-04 09:26:53 -0400

    j4p3

--- B/SummerSellOff.cpp
+++ B/SummerSellOff.cpp
@@ -18,12 +18,22 @@ int main()
     long long int intial_sum = 0;
     
     for (int i = 0; i < n; i++){
-        int a, b;
+        int a = 0; 
+        int b = 0;
         cin >> a >> b;
         d.push_back(make_pair(a, b));
+
+        //initial sum
         intial_sum += min(a, b);
-        int c;
-    
+
+        //diff
+        int c = 0;
+        c = 2*a - b; 
+        diff.push_back(c);
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << diff[i] << endl;
     }
 
 }
