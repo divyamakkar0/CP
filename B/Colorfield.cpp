bda0938803ccfa30541a61feda6d0d1739591364
Divya <divyamakkar000@gmail.com>
2024-07-09 09:04:45 -0400

    july 9

--- B/Colorfield.cpp
+++ B/Colorfield.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <set>
 #include <unordered_map>
 using namespace std;
 
@@ -12,19 +13,42 @@ int main()
     cin >> n >> m >> k >> t;
 
     int field[n][m];
-    vector<vector<int>> waste(k, vector<int>(2));
-    int queries[t][2];
 
+    set<pair<int, int>> waste;
     for(int i = 0; i < k; i++){
-        cin >> waste[k][0] >> waste[k][1];
+        int a, b;
+        cin >> a >> b;
+        waste.insert({a, b});
+    }
+ 
+    vector<pair<int, int>> queries(t);
+    for (int i = 0; i < t; ++i) {
+        int k, j;
+        cin >> k >> j;
+        queries[i] = {k, j};
     }
 
-    sort(waste.begin(), waste.end());
+    vector<string> crops = {"Carrots", "Kiwis", "Grapes"};
 
-    
-    for(int i = 0; i < t; i++){
-        cin >> queries[t][0] >> queries[t][1];
+    vector<int> waste_count(n);
+
+    for (int i = 0; i < t; i++){
+        int a = queries[i].first;
+        int b = queries[i].second;
+
+        if (waste.count({a, b}) != 0){
+            cout << "Waste" << endl;
+        } else {
+            int count = 0; 
+            for(const auto& w : waste){
+                if (w.first < a || (w.first == a && w.second < b)){
+                    count++;
+                }
+            }
+
+        int cell = ((a - 1) * m) + (b-1) - count;
+        cout << crops[cell % 3] << endl;
+        }
     }
-
 
 }
