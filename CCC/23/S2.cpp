8ec3bb5f5bed42d343288dc2535072eacebce96d
Divya <divyamakkar000@gmail.com>
2024-07-13 09:26:13 -0400

    july 13

---
+++ CCC/23/S2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> h(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> h[i];
+    }
+
+    
+
+}
