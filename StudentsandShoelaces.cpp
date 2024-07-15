41b496a5909fbdc0439b242b9841fa6c3d880455
Divya <divyamakkar000@gmail.com>
2024-07-15 10:58:55 -0400

    p1

--- StudentsandShoelaces.cpp
+++ StudentsandShoelaces.cpp
@@ -12,12 +12,15 @@ int main(){
     cin >> n >> m;
 
     int s[m][2];
-    vector<int> log;
+    vector<int> log(n);
+
     for(int i = 0; i < m; i++){
         int a, b;
         cin >> a >> b;
         s[i][0] = a;
         s[i][1] = b;
+        log[a]++;
+        log[b]++;
     }
 
     //try logging how many times each name shows up
