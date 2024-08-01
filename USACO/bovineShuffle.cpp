f828e62c02775b9703d81355cb2fc3db4ff128a7
Divya <divyamakkar000@gmail.com>
2024-08-01 11:14:01 -0400

    aug 1

--- USACO/bovineShuffle.cpp
+++ USACO/bovineShuffle.cpp
@@ -7,4 +7,32 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+
+    vector<int> perm(n);
+    vector<int> id(n);
+
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        a--;
+        id.push_back(a);
+    }
+
+    for(int i = 0; i < n; i++){
+        cin >> id[i];
+    }
+    ///////inputs taken in
+
+    int shuffleNum = 3;
+
+    vector<int> final(n);
+
+    for(int i = 0; i < shuffleNum; i++){
+        
+    }
+
+
+    
 }
