4eec5e5e893ea535c958682406780a3cc2df1d97
Divya <divyamakkar000@gmail.com>
2024-08-06 11:47:45 -0400

    aug 6 p2

---
+++ Codeforces/SlavicExam.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        string t;
+
+        cin >> s >> t;
+
+        for(int i = 0; i < s.size(); i++){
+            for(int j = 0; j < t.size(); j++){
+                
+            }
+        }
+
+        //see if all the characters needed in 
+
+    }
+}
