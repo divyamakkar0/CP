f284a2702e4d6905492ff153792aef866a14faae
Divya <divyamakkar000@gmail.com>
2024-08-11 11:11:25 -0400

    aug 11

---
+++ USACO/mowingField.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <numeric>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<pair<char, int>> mow(n);
+    for(int i = 0; i < n; i++){
+        cin >> mow[i].first >> mow[i].second;
+    }
+
+    
+}
