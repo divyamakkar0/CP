45a763704d1d5158ca932eadea46385135854699
Divya <divyamakkar000@gmail.com>
2024-08-13 09:51:58 -0400

    aug 13

---
+++ USACO/censoring.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    freopen("censor.in", "r", stdin);
+    freopen("censor.out", "w", stdout);
+    string s, t;
+    cin >> s >> t;
+
+    string output = ""; 
+    int tlen = t.length();
+
+    for (int i = 0; i < s.size(); i++) {
+        output += s[i];
+
+        if (output.size() >= tlen && output.substr(output.size() - tlen) == t) {
+            output.resize(output.size() - tlen);
+        }
+    }
+
+    cout << output << endl;
+}
