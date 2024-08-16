ff102b9916da3bc126192886f33578996f04c797
Divya <divyamakkar000@gmail.com>
2024-08-16 12:55:55 -0400

    aug 16

--- USACO/diamondCollector.cpp
+++ USACO/diamondCollector.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unordered_map>
 #include <limits.h>
+#include <cmath>
 #include <algorithm>
 using namespace std;
 
@@ -12,4 +13,22 @@ int main()
 
     int n, k;
     cin >> n >> k;
+
+    vector<int> sizes(n);
+    for(int i = 0; i < n; i++){
+        cin >> sizes[i];
+    }
+
+    int maxval = 0;
+    for(int i = 0; i < n; i++){
+        int count = 0; 
+        for(int j = 0; j < n; j++){ 
+            if (sizes[i] <= sizes[j] && sizes[j] <= sizes[i] + k){
+                count++;
+            }
+        }
+        maxval = max(maxval, count);
+    }
+
+    cout << maxval;
 }
