ca59f97590242d887b0f6db06a066cd4f8307d4e
divyamakkar0 <divyamakkar000@gmail.com>
2025-01-01 12:05:28 -0500

    jan 1

--- Codeforces/completetheword.cpp
+++ Codeforces/completetheword.cpp
@@ -1,55 +1,61 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <unordered_map>
 using namespace std;
 
-int main()
-{
+int main() {
     string s;
-    cin >> s; 
+    cin >> s;
 
-    string final;
+    bool found = false;
 
+    for (int i = 0; i <= (int(s.length()) - 26); i++) {
+        vector<int> alphabet(26, 0);
+        int missing = 0, questionMarks = 0;
 
-    for (int i = 0; i < (s.length() - 26); i++){
-        int q = 0;
-        int z = 0;
-        vector<char> empty;
-        vector<int> alphabet;
-
-        for(int j = 0; j < 26; j++){
-            char a = s[j];
-            cout << a << endl;
-            if (a == '?'){
-                q++;
+        for (int j = 0; j < 26; j++) {
+            char c = s[i + j];
+            if (c == '?') {
+                questionMarks++;
             } else {
-                alphabet[a - 'A']++;
-            };
-        }
-        for (int k = 0; k < 26; k++){
-            if (alphabet[k] == 0){
-                cout << alphabet[k] << endl;
-                z++;
-                empty.push_back(alphabet[k]);
+                alphabet[c - 'A']++;
+                if (alphabet[c - 'A'] > 1) break; 
             }
         }
-        if (z <= q){
-            for (int m = 0; m < 26; m++){
-                int ind = 0;
-                if (s[m] == '?'){
-                    final += empty[ind];
-                    ind++;
-                } else {
-                    final += s[m];
+
+        for (int k = 0; k < 26; k++) {
+            if (alphabet[k] == 0) missing++;
+        }
+
+        if (missing <= questionMarks) {
+            string result = s;
+            vector<char> unused;
+            for (int k = 0; k < 26; k++) {
+                if (alphabet[k] == 0) unused.push_back(char('A' + k));
+            }
+
+            int index = 0;
+            for (int j = 0; j < 26; j++) {
+                if (result[i + j] == '?') {
+                    result[i + j] = unused[index++];
                 }
             }
-            cout << final;
+
+            
+            for (char &c : result) {
+                if (c == '?') {
+                    c = 'A'; 
+            }}
+
+            cout << result << endl;
+            found = true;
             break;
         }
-        cout << -1;
     }
+
+    if (!found) {
+        cout << -1 << endl;
+    }
+
+
 }
