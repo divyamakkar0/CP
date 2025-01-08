09cb2b2affcf35705052de6634151cb5f044ad47
divyamakkar0 <divyamakkar000@gmail.com>
2025-01-08 07:19:05 -0500

    jan 8

--- Codeforces/SeaandIslands.cpp
+++ Codeforces/SeaandIslands.cpp
@@ -7,69 +7,31 @@
 #include <unordered_map>
 using namespace std;
 
-int main()
-{
+#include <iostream>
+using namespace std;
+
+int main() {
     int n, k;
     cin >> n >> k;
-    
-    if (n % 2 == 0){
-        if (k > (pow(n, 2) / 2)){
-            cout << "NO" << endl; 
-        } else {
-            int counter = 0;
-            cout << "YES" << endl;
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < n; j++){
-                    if (k <= 0){
-                        cout << "S";
-                    } else {
-                        if (counter % 2 == 0){
-                        cout << "L";
-                        k--;
-                    } else {
-                        cout <<"S";
-                    }
-                    counter++;
-                    }
+
+    int maxL = (n * n + 1) / 2; 
+
+    if (k > maxL) {
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (k > 0 && (i + j) % 2 == 0) { 
+                    cout << "L";
+                    k--;
+                } else {
+                    cout << "S";
                 }
-                cout << endl;
-            } 
-    }}
-    else {
-        if (k > (pow((n+1), 2) / 2)){
-            cout << "NO" << endl;
-        } else {
-            int counter = 0;
-            cout << "YES" << endl;
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < n; j++){
-                    if (k <= 0){
-                        cout << "S";
-                    } else if (i % 2 != 0 && j == 0){
-                        counter++;
-                        if (counter % 2 == 0){
-                        cout << "L";
-                        k--;
-                        } else {
-                        cout <<"S";
-                        }
-                        counter++;
-                    } else {
-                        if (counter % 2 == 0){
-                        cout << "L";
-                        k--;
-                        } else {
-                        cout <<"S";
-                        }
-                        counter++;                   
-                     }
-                }
-                cout << endl;
             }
+            cout << endl;
         }
     }
 
-
+    return 0;
 }
-
-
