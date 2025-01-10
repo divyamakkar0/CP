f019ffb29f5311d699541a6ebf8c73e443e55b02
divyamakkar0 <divyamakkar000@gmail.com>
2025-01-10 13:47:58 -0500

    jan 10

--- Codeforces/setstrings.cpp
+++ Codeforces/setstrings.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <unordered_map>
 using namespace std;
 
-int main()
-{
+int main() {
     int k;
     string q;
+    vector<int> alphabet(26, 0); 
+    int diff = 0;
 
     cin >> k >> q;
 
-    if (k > q.length()){
-        cout << "NO";
-    } else {
-        int count = 1;
-        char first = q[0];
-        for (int i = 0; i < q.length(); i++){
-            if (count == k){
-                cout << q[i];
-            } else {
-                if (q[i] != first){
-                    cout << endl << q[i];
-                    first = q[i];
-                    count++;
-                } else {
-                    cout << q[i];
-                }
-            }
+    for (int i = 0; i < q.size(); i++) {
+        if (alphabet[q[i] - 'a'] == 0) {
+            diff++;
         }
+        alphabet[q[i] - 'a']++;
+    }
+
+    if (k > diff) {
+        cout << "NO" << endl;
+        return 0;
+    }
+
+    cout << "YES" << endl;
+
+    int count = 0;
+    vector<bool> used(26, false);
+    for (int i = 0; i < q.size(); i++) {
+        if (count < k && !used[q[i] - 'a']) {
+            if (count > 0) {
+                cout << endl; 
+            }
+            count++;
+            used[q[i] - 'a'] = true;
+        }
+        cout << q[i];
     }
     cout << endl;
+
+    return 0;
 }
+
