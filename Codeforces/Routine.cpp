d88f944c1efb4ab852de0dc478e8374a6caa6243
divyamakkar0 <divyamakkar000@gmail.com>
2025-04-21 08:46:41 -0400

    aprl 20

---
+++ Codeforces/Routine.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int n = min(a,b);
+    int area = c * d;
+    
+
+
+
+
+
+
+
+}
+
