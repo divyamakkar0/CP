ed193061c57b7c83cda1b18b43b235a5dac24041
divyamakkar0 <divyamakkar000@gmail.com>
2025-04-27 08:42:12 -0400

    apr 27

--- Codeforces/Casino.cpp
+++ Codeforces/Casino.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <map>
-#include <string>
+#include <cmath> 
 using namespace std;
 
 int main()
@@ -14,28 +12,23 @@ int main()
         int n, m;
         cin >> n >> m;
 
-        int games[n][m];
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
+        vector<vector<int>> games(n, vector<int>(m));
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
                 cin >> games[i][j];
             }
-            // sort(games[i], games[i] + m);
         }
 
-        int sum = 0;
-        for(int col = 0; col < m; col++){
-            for(int i = 0; i < n-1; i++){
-                for(int j = i+1; j < n; j++){
-                    // cout << games[i][col] << " " << games[j][col] << endl; 
-                    sum += abs(games[i][col] - games[j][col]);
+        long long sum = 0; 
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                for(int k = 0; k < m; k++){
+                    sum += abs(games[i][k] - games[j][k]);
                 }
             }
-            // cout << endl;
         }
 
         cout << sum << endl;
     }
-
 }
