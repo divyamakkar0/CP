03e3ba2d28730b47c79fb87a55e38dbca7a6895f
divyamakkar0 <divyamakkar000@gmail.com>
2025-05-09 08:17:27 -0400

    may 9

--- USACO/angryCows.cpp
+++ USACO/angryCows.cpp
@@ -10,41 +10,62 @@ int main()
 {
     int n;
     cin >> n;
-    vector<int> bales(n) :
-
-                           for (int i = 0; i < n; i++) cin >> bales[i];
-
+    vector<int> bales(n);
+    for(int i = 0; i < n; i++) cin >> bales[i];
     sort(bales.begin(), bales.end());
+
 
     // we do this for every cow and simulate the explosion
     int max_explosions = 0;
     for (int i = 0; i < n; i++)
     {
+        
         int bale_counter = 1;
         int radius = 1;
         int lb = i - radius;
         int ub = i + radius;
 
+        cout << i << " ______" << endl;
+        int current = i;
+
         // simulate explosion to the left
         while (lb >= 0)
         {
-            int prev = i;
-            if (bales[prev] + radius = bales[lb])
-            {
-                bale_counter++;
-                radius += 1;
-                prev = lb;
-                lb = i - radius;
+            for(int j = 0; j < lb; j++){
+                if (bales[current] - bales[j] <= radius)
+                {
+                    bale_counter++;
+                    radius += 1;
+                    current = lb;
+                    lb = current - radius;
+                }
+                else
+                {
+                    break;
+                }
             }
-            else
-            {
-                break;
-            }
+            
+            cout << "current " << current << endl;
+            cout << "bale counter: " << bale_counter << " radius: " << radius << " lb: " << lb << " ub: " <<  ub << endl;
         }
 
-        while (ub < n)
-        {
-            radius = 1;
-        }
+        // while (ub < n)
+        // {
+        //     int current = i; 
+        //     if (bales[current] + radius == bales[ub]){
+        //         bale_counter++;
+        //         radius += 1;
+        //         current = ub;
+        //         ub = current + radius;
+        //     } 
+        //     else{
+        //         break;
+        //     }
+        //     // cout << "current " << current << endl;
+        //     // cout << "bale counter: " << bale_counter << " radius: " << radius << " lb: " << lb << " ub: " <<  ub << endl;
+        // }
+        // max_explosions = max(max_explosions, bale_counter);
     }
+
+    // cout << max_explosions << endl;
 }
