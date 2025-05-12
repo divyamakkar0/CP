d2336347f245a69196c1f2abdd25f686c84c70c0
divyamakkar0 <divyamakkar000@gmail.com>
2025-05-11 20:26:48 -0400

    may 11

---
+++ USACO/outofPlace.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> order(n) : for (int i = 0; i < n; i++) cin >> order[i];
+
+    int sorts = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (order[i] = < order[i + 1])
+        {
+            continue;
+        }
+        else
+        {
+            // 2 8 3 7 9
+            // 2 3 8 7 9
+            // 2 3 7 8 9
+            //
+        }
+        if (i == n - 1)
+        {
+            if (order[i] >= order[i - 1])
+            {
+                continue:
+            }
+            else
+            {
+                for (int j =)
+            }
+        }
+    }
+}
