c3895d343bc4c595c58bd835943e911266d34700
divyamakkar0 <divyamakkar000@gmail.com>
2025-08-13 09:14:59 -0400

    aug 13

--- Codeforces/chocolate.cpp
+++ Codeforces/chocolate.cpp
@@ -1,41 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <cmath>
 #include <algorithm>
 #include <string>
 using namespace std;
 
 int main(){
-            int n;
-           cin >> n;
+    int n;
+    cin >> n;
+    vector<int> bar;
+    for(int i = 0; i < n; i++){
+        int l;
+        cin >> l;
+        bar.emplace_back(l);
+    }
 
-           int count = 0;
-           vector<int> chocolates(n);
-           for (int i = 0; i < n; i++) {
-               cin >> chocolates[i];
-               if (chocolates[i] == 1)
-               {
-                   count++;
-               }
-           }
-
-           int mult = 1;
-           int c = 0;
-           if (count == 1) {
-               cout << 1 << endl;
-           } else {
-               for (int i = 0; i < n; i++)
-               {
-                   if (chocolates[i] == 1)
-                   {
-                       mult *= (c + 1);
-                       c = 0;
-                   }
-                   else
-                   {
-                       c++;
-                   }
-               }
-               cout << mult << endl;
-           }
-
+    int total = 1;
+    int index = 0; int back_ind = n-1;
+    while(bar[index] != 1 && index < n-1) index++;
+    cout << index;
+    
+    while(bar[back_ind] != 1 && back_ind >= 0) back_ind--; 
+    if (index != n-1) index += 1; 
+    if (back_ind != 0) back_ind -=1;
+    int between = 0;
+    for(int index; index < back_ind; index++){
+        if(bar[index] == 1 && between != 0){
+            total *= between;
+            between = 0;
+        } 
+        if (bar[index] == 0) between++;
+        cout << "between :" << between << endl;
+    }
+    return total;
+    
 }
---
+++ Codeforces/george.py
@@ -0,0 +1,8 @@
+n = input()
+count = 0
+for i in range(int(n)):
+    p, q = map(int, input().split())
+    if q - p >= 2:
+        count += 1
+
+print(count)
