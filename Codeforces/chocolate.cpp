6032ec3311e71c3c31af814711c4fefc1981ddd1
divyamakkar0 <divyamakkar000@gmail.com>
2025-08-20 09:12:09 -0400

    aug 20

--- Codeforces/chocolate.cpp
+++ Codeforces/chocolate.cpp
@@ -15,24 +15,58 @@ int main(){
         cin >> l;
         bar.emplace_back(l);
     }
+    bool allzeros = true;
+    for(int i = 0; i < n; i++){
+        if (bar[i] != 0){
+            allzeros = false;
+            break;
+        }
+    }
+
+    if (!allzeros){
+        //elim all leading ad trailing 0s
+    int index = 0; int back_ind = n-1;
+    while(index < n-1 && bar[index] != 1) index++;
+    while(back_ind >= 0 && bar[back_ind] != 1) back_ind--;
+    // cout << index << " " << back_ind << endl;
 
     int total = 1;
-    int index = 0; int back_ind = n-1;
-    while(bar[index] != 1 && index < n-1) index++;
-    cout << index;
-    
-    while(bar[back_ind] != 1 && back_ind >= 0) back_ind--; 
-    if (index != n-1) index += 1; 
-    if (back_ind != 0) back_ind -=1;
-    int between = 0;
-    for(int index; index < back_ind; index++){
-        if(bar[index] == 1 && between != 0){
-            total *= between;
-            between = 0;
-        } 
-        if (bar[index] == 0) between++;
-        cout << "between :" << between << endl;
+    int empty = 0;
+    for(int i = index + 1; i < back_ind; i++){
+        // cout << "i : " << i << " total: " << total << endl;
+        if (bar[i] == 1) {
+            // cout << "index at one: " << i << endl;
+            // cout << "count: " << empty << endl;
+            // cout << endl;
+            if (empty != 0){
+                int combinations = 2;
+                empty -= 1;
+                while(empty > 0){
+                    combinations += 1;
+                    empty -= 1;
+                }
+                empty = 0;
+                total *= combinations;
+            }
+        } else {
+            // cout << "index is zero: " << i << endl;
+            // cout << "count: " << empty << endl;
+            // cout << endl;
+            empty += 1;
+        }
     }
-    return total;
-    
+        if (empty != 0){
+            int combinations = 2;
+            empty -= 1;
+            while(empty > 0){
+                combinations += 1;
+                empty -= 1;
+            }
+            total *= combinations;
+        }
+
+        cout << total << endl;
+    } else {
+        cout << 0 << endl;
+    }
 }
