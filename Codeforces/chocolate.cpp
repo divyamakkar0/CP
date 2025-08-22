627c2ff39920547cc8234287c1f032990efef62f
divyamakkar0 <divyamakkar000@gmail.com>
2025-08-22 11:31:04 -0400

    aug 22

--- Codeforces/chocolate.cpp
+++ Codeforces/chocolate.cpp
@@ -25,38 +25,39 @@ int main(){
 
     if (!allzeros){
         //elim all leading ad trailing 0s
-    int index = 0; int back_ind = n-1;
-    while(index < n-1 && bar[index] != 1) index++;
-    while(back_ind >= 0 && bar[back_ind] != 1) back_ind--;
-    // cout << index << " " << back_ind << endl;
+        int index = 0; 
+        int back_ind = n-1;
+        while(index < n-1 && bar[index] != 1) index++;
+        while(back_ind >= 0 && bar[back_ind] != 1) back_ind--;
+        // cout << index << " " << back_ind << endl;
 
-    int total = 1;
-    int empty = 0;
-    for(int i = index + 1; i < back_ind; i++){
-        // cout << "i : " << i << " total: " << total << endl;
-        if (bar[i] == 1) {
-            // cout << "index at one: " << i << endl;
-            // cout << "count: " << empty << endl;
-            // cout << endl;
-            if (empty != 0){
-                int combinations = 2;
-                empty -= 1;
-                while(empty > 0){
-                    combinations += 1;
+        long long total = 1;   
+        long long empty = 0;  
+        for(int i = index + 1; i < back_ind; i++){
+            // cout << "i : " << i << " total: " << total << endl;
+            if (bar[i] == 1) {
+                // cout << "index at one: " << i << endl;
+                // cout << "count: " << empty << endl;
+                // cout << endl;
+                if (empty != 0){
+                    long long combinations = 2;   
                     empty -= 1;
+                    while(empty > 0){
+                        combinations += 1;
+                        empty -= 1;
+                    }
+                    empty = 0;
+                    total *= combinations;
                 }
-                empty = 0;
-                total *= combinations;
+            } else {
+                // cout << "index is zero: " << i << endl;
+                // cout << "count: " << empty << endl;
+                // cout << endl;
+                empty += 1;
             }
-        } else {
-            // cout << "index is zero: " << i << endl;
-            // cout << "count: " << empty << endl;
-            // cout << endl;
-            empty += 1;
         }
-    }
         if (empty != 0){
-            int combinations = 2;
+            long long combinations = 2;   
             empty -= 1;
             while(empty > 0){
                 combinations += 1;
